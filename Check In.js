b4f9a0e52cc867dbbf4ef4a1f764f59f121d41d1
kolbydayley <kolbydayley@gmail.com>
2021-10-05 11:38:08 -0600

    Update Check In.js

--- Check In.js
+++ Check In.js
@@ -1,4 +1,4 @@
-// Variables used by Scriptable.
+// Variables used by Scriptable. Variables may exist above this line if the tool was set up with the iOS installer shortcut
 
 
 
@@ -22,14 +22,6 @@ var finalText; // init where the final text message will be stored
 var date = new Date(); // set date for logging
 
 
-//titleStorage.push("Responsible for Paige");
-//titleStorage.push("Got support");
-//titleStorage.push("Dailies");
-//titleStorage.push("Lust");
-//titleStorage.push("Social media");
-//titleStorage.push("Boasted");
-//titleStorage.push("Spanish time");
-
 let alert = new Alert(); // create the alert object
 alert.title = "Check In";  // Set title
 for (var i in CHECK_IN_AREAS) { // fill the alert fields with the CHECK_IN_AREAS defined above
@@ -41,7 +33,6 @@ if (await alert.present() == 0) {
     for (var i in CHECK_IN_AREAS) {
         textStorage.push(alert.textFieldValue(i));
     }
-    log(textStorage);
     for (var i in textStorage) {  // build the rows to be appended to the storage file
         if (i == 0) {
             finalSt = date + "," + titleStorage[i] + "," + textStorage[i] + "\n";
@@ -49,8 +40,6 @@ if (await alert.present() == 0) {
             finalSt = finalSt + date + "," + titleStorage[i] + "," + textStorage[i] + "\n";
         }
     }
-    log(finalSt);
-    log(textStorage);
     for (var j in textStorage) { // build the text message
         if (j == 0) {
             finalText = titleStorage[j] + ":" + textStorage[j] + "\n";
@@ -58,15 +47,12 @@ if (await alert.present() == 0) {
             finalText = finalText + titleStorage[j] + ":" + textStorage[j] + "\n";
         }
     }
-    log(finalText);
     let fm = await FileManager.iCloud();  // build file manager
     let path = await fm.bookmarkedPath("Check-in"); // find the file that is associated with the bookmark that is defined in the settings of the Scriptable app, titled "Check-in" in the File Bookmarks section
     var cont = fm.readString(path);  // get current file
-    log(cont);
     let nCont = cont + finalSt; // add today's check-in to the file
     await fm.writeString(path, nCont);  // update the file with the newly formed check-in
     let f = fm.read(path);
-    log(f);
 
     var mes = new Message(); // build text message
     mes.body = finalText;
