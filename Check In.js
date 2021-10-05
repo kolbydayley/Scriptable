e6088f449372ef59670a1534455ec4b4db5f03e2
kolbydayley <kolbydayley@gmail.com>
2021-10-05 16:41:23 -0600

    Update Check In.js

--- Check In.js
+++ Check In.js
@@ -16,16 +16,27 @@ const CHECK_IN_AREAS = [
 // ------------------------------------------------------
 // END CONSANTS
 
+
 var textStorage = []; // init storage for the text message
 var finalSt; // init storage for the file on which your check-in will be stored
 var finalText; // init where the final text message will be stored
 var date = new Date(); // set date for logging
 
 
+//titleStorage.push("Responsible for Paige");
+//titleStorage.push("Got support");
+//titleStorage.push("Dailies");
+//titleStorage.push("Lust");
+//titleStorage.push("Social media");
+//titleStorage.push("Boasted");
+//titleStorage.push("Spanish time");
+
 let alert = new Alert(); // create the alert object
 alert.title = "Check In";  // Set title
-for (var i in CHECK_IN_AREAS) { // fill the alert fields with the CHECK_IN_AREAS defined above
-    alert.addTextField(CHECK_IN_AREAS[i]);
+for (var i in CHECK_IN_AREAS) { // fill the alert fields with the CHECK_IN_AREAS defined above	
+
+	var t = CHECK_IN_AREAS[i];
+    alert.addTextField(t).setNumbersAndPunctuationKeyboard();
 }
 alert.addAction("Submit");
 alert.addCancelAction("Cancel");
@@ -33,26 +44,32 @@ if (await alert.present() == 0) {
     for (var i in CHECK_IN_AREAS) {
         textStorage.push(alert.textFieldValue(i));
     }
+    log(textStorage);
     for (var i in textStorage) {  // build the rows to be appended to the storage file
         if (i == 0) {
-            finalSt = date + "," + titleStorage[i] + "," + textStorage[i] + "\n";
+            finalSt = date + "," + CHECK_IN_AREAS[i] + "," + textStorage[i] + "\n";
         } else {
-            finalSt = finalSt + date + "," + titleStorage[i] + "," + textStorage[i] + "\n";
+            finalSt = finalSt + date + "," + CHECK_IN_AREAS[i] + "," + textStorage[i] + "\n";
         }
     }
+    log(finalSt);
+    log(textStorage);
     for (var j in textStorage) { // build the text message
         if (j == 0) {
-            finalText = titleStorage[j] + ":" + textStorage[j] + "\n";
+            finalText = CHECK_IN_AREAS[j] + ":" + textStorage[j] + "\n";
         } else {
-            finalText = finalText + titleStorage[j] + ":" + textStorage[j] + "\n";
+            finalText = finalText + CHECK_IN_AREAS[j] + ":" + textStorage[j] + "\n";
         }
     }
+    log(finalText);
     let fm = await FileManager.iCloud();  // build file manager
     let path = await fm.bookmarkedPath("Checkin"); // find the file that is associated with the bookmark that is defined in the settings of the Scriptable app, titled "Check-in" in the File Bookmarks section
     var cont = fm.readString(path);  // get current file
+    log(cont);
     let nCont = cont + finalSt; // add today's check-in to the file
     await fm.writeString(path, nCont);  // update the file with the newly formed check-in
     let f = fm.read(path);
+    log(f);
 
     var mes = new Message(); // build text message
     mes.body = finalText;
