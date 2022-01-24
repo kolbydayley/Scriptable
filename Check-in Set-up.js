32f8d9165506160756ae28c664231f3908979937
kolbydayley <kolbydayley@gmail.com>
2022-01-24 16:54:56 -0700

    Update Check-in Set-up.js

--- Check-in Set-up.js
+++ Check-in Set-up.js
@@ -1,11 +1,13 @@
-const gitHubUrl = "https://raw.githubusercontent.com/kolbydayley/Scriptable/main/Check%20In.js";
-var req = new Request(gitHubUrl);
-var codeString = await req.loadString();
-var fm = await FileManager.iCloud();
-const path = fm.joinPath(fm.documentsDirectory(), "Checkin.js");
-fm.writeString(path, codeString);
-var url = encodeURI("scriptable:///run/Untitled%20Script");
-console.log(url)
-Safari.open(url);
+async function setUp(){
+	const gitHubUrl = "https://raw.githubusercontent.com/kolbydayley/Scriptable/main/Check%20In.js";
+	var req = new Request(gitHubUrl);
+	var codeString = await req.loadString();
+	var fm = await FileManager.iCloud();
+	const path = fm.joinPath(fm.documentsDirectory(), "Checkin.js");
+	await fm.writeString(path, codeString);
+	var url = encodeURI("scriptable:///run/Untitled%20Script");
+	console.log(url)
+	Safari.open(url);
+}
 
 	
