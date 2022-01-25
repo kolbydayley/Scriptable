4ab55b8f442a7dec2d53b6652768c603f2642a16
kolbydayley <kolbydayley@gmail.com>
2022-01-25 14:33:15 -0700

    Update Check In.js

--- Check In.js
+++ Check In.js
@@ -1,3 +1,6 @@
+
+//FUNCTION TO SET A STRING TO TITLE CASE
+//---------------------------------------------
 function toTitleCase(str) {
     return str.replace(
         /\w\S*/g,
@@ -6,11 +9,14 @@ function toTitleCase(str) {
         }
     );
 }
+//---------------------------------------------
 
+
+//FUNCTION TO CREATE A DEFAULT SETTINGS OBJECT IN THE CASE OF A RESET, NO RETURN VALUE
+//---------------------------------------------
 function createSettingsDefault(filePath) {
     var fm = FileManager.local();
     var dir = fm.libraryDirectory();
-    //console.log(fm.listContents(dir));
     var path = fm.joinPath(dir, "check_in_tracker_settings.txt");
     var data = {
         checkInItems: [
@@ -27,13 +33,15 @@ function createSettingsDefault(filePath) {
     fm.writeString(path, string);
     var cont = fm.readString(path);
     var ps = JSON.parse(cont)
-    console.log(ps);
 }
+//---------------------------------------------
 
+
+//FUNCTION TO UPDATE THE SETTINGS, RETURNS NO VALUES
+//---------------------------------------------
 function updateSettings(items, contact, filePath, bmAdded) {
     var fm = FileManager.local();
     var dir = fm.libraryDirectory();
-    //console.log(fm.listContents(dir));
     var path = fm.joinPath(dir, "check_in_tracker_settings.txt");
     var data = {
         checkInItems: items,
@@ -45,41 +53,50 @@ function updateSettings(items, contact, filePath, bmAdded) {
     fm.writeString(path, string);
     var cont = fm.readString(path);
     var ps = JSON.parse(cont)
-    console.log(ps);
 }
+//---------------------------------------------
 
+
+//FUNCTION TO GET ALL SETTINGS, RETURNS A JSON OBJECT OF THE SETTINGS
+//---------------------------------------------
 function getSettings() {
     var fm = FileManager.local();
     var dir = fm.libraryDirectory();
-    //console.log(fm.listContents(dir));
     var path = fm.joinPath(dir, "check_in_tracker_settings.txt");
     var cont = fm.readString(path);
     var ps = JSON.parse(cont)
-    //console.log(ps.checkInItems + "   " + ps.accountabilityContact + "   " + ps.checkInSheetPath);
     return ps;
 }
+//---------------------------------------------
 
+
+//FUNCTION TO RUN A CHECK-IN
+//---------------------------------------------
 async function runCheckIn(settings) {
-    //console.log(settings);	
+
+   //Initialize variables
     var textStorage = []; // init storage for the text message
     var finalSt; // init storage for the file on which your check-in will be stored
     var finalText; // init where the final text message will be stored
     var date = new Date(); // set date for logging
 
+  //Create check-in alert
     let alert = new Alert(); // create the alert object
     alert.title = "Check In"; // Set title
     for (var i in CHECK_IN_AREAS) { // fill the alert fields with the CHECK_IN_AREAS defined above	
-
         var t = CHECK_IN_AREAS[i];
         alert.addTextField(t).setNumbersAndPunctuationKeyboard();
     }
     alert.addAction("Submit");
     alert.addCancelAction("Cancel");
+
+    //Present alert then take data and preps it for storage
     if (await alert.present() == 0) {
         for (var i in CHECK_IN_AREAS) {
             textStorage.push(alert.textFieldValue(i));
         }
-        log(textStorage);
+
+      //Preps for storage in file
         for (var i in textStorage) { // build the rows to be appended to the storage file
             if (i == 0) {
                 finalSt = date + "," + CHECK_IN_AREAS[i] + "," + textStorage[i] + "\n";
@@ -87,32 +104,29 @@ async function runCheckIn(settings) {
                 finalSt = finalSt + date + "," + CHECK_IN_AREAS[i] + "," + textStorage[i] + "\n";
             }
         }
-        log(finalSt);
-        log(textStorage);
+
+        //Preps for storage in text message
         for (var j in textStorage) { // build the text message
             if (j == 0) {
                 finalText = CHECK_IN_AREAS[j] + ":" + textStorage[j] + "\n";
-            } else {
+            } else if(j < textStorage.length-1) {
                 finalText = finalText + CHECK_IN_AREAS[j] + ":" + textStorage[j] + "\n";
+            } else{
+              finalText = finalText + CHECK_IN_AREAS[j] + ":" + textStorage[j];
             }
         }
-        console.log(finalText);
-        //console.log(path);
-        let iCloudInUse = true;
-        //console.log("iCloud: " + iCloudInUse);
-        let fm = await iCloudInUse ? await FileManager.iCloud() : await FileManager.local()
+      //Start file storage initialization
+        let fm = FileManager.local();
+        let iCloudInUse = fm.isFileStoredIniCloud(fm.bookmarkedPath("Checkin"));
+        fm = await iCloudInUse ? await FileManager.iCloud() : await FileManager.local()
         let path = fm.bookmarkedPath("Checkin");
         fm.downloadFileFromiCloud(path);
-        // find the file that is associated with the bookmark that is defined in the settings of the Scriptable app, titled "Check-in" in the File Bookmarks section
-        console.log("Made it past FM");
         var cont = fm.readString(path); // get current file
-        console.log("cont " + cont);
         let nCont = cont + finalSt; // add today's check-in to the file
         await fm.writeString(path, nCont); // update the file with the newly formed check-in
-        let f = fm.read(path);
-        console.log("file contents " + f);
-
-        var mes = new Message(); // build text message
+       
+       //Build text message
+        var mes = new Message();
         mes.body = finalText;
         mes.recipients = [PARTNER];
         mes.send();
@@ -120,26 +134,32 @@ async function runCheckIn(settings) {
         log("Canceled")
     }
 }
+//---------------------------------------------
 
+
+//FUNCTION TO SELECT A NEW CONTACT FOR THE DAILY ACCOUNTABILITY CONTACT VARIABLE
+//---------------------------------------------
 async function selectContact(contact) {
-	var c = Array.from(contact);
-	var partnerNum = c[0]
-    var partnerName = c[1]
-    var cont = await ContactsContainer.all();
-    //log(cont);
-    var c = await Contact.all(cont);
-    //log(c)
-    var contacts = [];
+  //Initialize variables
+    var partnerNum = null;
+    var partnerName = null;
+    if (contact) {
+        var c = Array.from(contact);
+        partnerNum = c[0]
+        partnerName = c[1]
+    }
 
+  // Build contacts container, gets all contacts from the contact object and prepares them for the selection window
+    var cont = await ContactsContainer.all();
+    var c = await Contact.all(cont);
+    var contacts = [];
     for (var i in c) {
         var name = c[i].givenName + " " + c[i].familyName,
             pns = c[i].phoneNumbers;
-        //log(pns);
         for (var j in pns) {
             var s = pns[j];
             var v = s.value;
             var n = s.localizedLabel;
-            //log(v);
             if (v != "") {
                 var val = [name, n + ":" + v, v];
                 contacts.push(val);
@@ -147,6 +167,7 @@ async function selectContact(contact) {
         }
     }
 
+  //Build and present contactselection menu
     const menu = contacts;
     contacts.sort()
     var table = new UITable();
@@ -155,7 +176,6 @@ async function selectContact(contact) {
     r.addText("Please select accountability partner");
     r.isHeader = true;
     table.addRow(r);
-
     for (var i in menu) {
         const row = new UITableRow()
         const name = menu[i][0];
@@ -164,16 +184,19 @@ async function selectContact(contact) {
         row.onSelect = async () => {
             partnerNum = num;
             partnerName = name;
-
-            //log("selection"+selection);
         }
         row.addText(name, sub)
         table.addRow(row);
     }
     await table.present();
+
     return [partnerNum, partnerName];
 }
+//---------------------------------------------
 
+
+//FUNCTION TO ADD OR CHANGE ITEMS INCLUDED IN THE DAILY CHECK-IN
+//---------------------------------------------
 async function addCheckInItems(cii) {
     var cis = [];
     var al = new Alert();
@@ -184,8 +207,6 @@ async function addCheckInItems(cii) {
     al.addCancelAction("Cancel")
     if (await al.present() == 0) {
         var val = await al.textFieldValue(0);
-        //log(val);
-
         var split = val.split(",");
         for (var i in split) {
             cis.push(toTitleCase(split[i]));
@@ -193,23 +214,30 @@ async function addCheckInItems(cii) {
     }
     return cis;
 }
+//---------------------------------------------
 
 
+//CHANGES NOTIFICATION TIME, NO OUTPUTS
+//---------------------------------------------
 async function changeNotificationTime() {
+  //Build and present alert warning before the time selector pops
     var a = new Alert();
     a.title = "You will now select the time at which you would like to be presented your check-in notification. Press Next."
     a.addAction("Next");
     await a.present();
+
+  //Build and present the DatePicker
     let dp = new DatePicker();
     var time = await dp.pickTime();
-    log(time);
+
+    //formats the result of the data picker
     let df = new DateFormatter();
     df.dateFormat = "H";
     var hour = df.string(time);
-    log(hour);
     df.dateFormat = "m";
     var minute = df.string(time);
-    log(minute);
+
+  //Build and set a new daily notification, removes the old one first
     Notification.removePending(["scriptable-check-in"])
     var n = new Notification();
     n.title = "Check-in 📈";
@@ -220,23 +248,29 @@ async function changeNotificationTime() {
     minute = parseInt(minute)
     n.setDailyTrigger(hour, minute, true);
     n.schedule();
-}
 
+}
+//---------------------------------------------
+
+
+//RUNS MAIN ROLLUP MENU, PRESENTS OPTIONS FOR USE, DOES NOT RETURN ANYTHING
+//---------------------------------------------
 async function runMenu(settings) {
-    console.log("Made it into run menu")
+
+   //Builds and presents menu
     let menu = new Alert()
-    console.log("settings" + settings)
     menu.addCancelAction('Exit')
     menu.addAction('Check In')
-    menu.addAction('Change check-in time')
-    menu.addAction('Change accountability contact')
-    menu.addAction("Change check-in items")
-    menu.addAction('Show spreadsheet')
-    menu.addAction('Update check-in storage location')
-    menu.addAction('Reset all')
+    menu.addAction('Change Check-in Time')
+    menu.addAction('Change Accountability Contact')
+    menu.addAction("Change Check-in Items")
+    menu.addAction('Show Spreadsheet')
+    menu.addAction('Update Check-in Storage File')
+    menu.addAction('Reset All')
     menu.title = 'What would you like to do?'
     let menuOutput = await menu.presentSheet()
-    log(menuOutput)
+
+  //Run switch function pased on menu selection
     switch (menuOutput) {
         case 0:
             await runCheckIn(settings)
@@ -250,10 +284,8 @@ async function runMenu(settings) {
             break
         case 3:
             var cii = await addCheckInItems();
-            console.log("cii" + toString(cii));
-            if(cii[0] == null){
-              cii = settings.checkInItems;
-              console.log("Add Check in Canceled")
+            if (cii[0] == null) {
+                cii = settings.checkInItems;
             }
             await updateSettings(cii, settings.accountabilityContact, settings.checkInSheetPath);
             break
@@ -271,34 +303,37 @@ async function runMenu(settings) {
             break
     }
 }
+//---------------------------------------------
 
-async function selectPath() {
-    var dir = await DocumentPicker.open();
-    return dir;
-}
 
+//TAKES FILES PATH AND OPENS FILE IN SAFARI
+//---------------------------------------------
 async function openFile(path) {
-    console.log(path);
     var url = encodeURI("shareddocuments://" + path);
-    console.log(url)
     Safari.open(url);
     Script.complete()
 }
+//---------------------------------------------
 
+
+//UPDATES BOOKMARK BY EITHER DOWNLOADING, OR TRIGGERING A APPLE SHORTCUT NEEDED FOR THE PURPOSE
+//---------------------------------------------
 async function updateBookmark(settings) {
     var bmAdded = settings.bookmarkAdded || "false";
-    console.log(bmAdded);
     if (!bmAdded || bmAdded === "false") {
         var url = encodeURI("https://www.icloud.com/shortcuts/6987686f9e1947b18fc531fa1e72e224");
         Safari.open(url);
         Script.complete()
     } else {
         var url = encodeURI("shortcuts://run-shortcut?name=Create Scriptable Bookmark");
-        console.log(url)
         Safari.open(url);
     }
 }
+//---------------------------------------------
 
+
+// GENERAL ALERT, NOT IN USE
+//---------------------------------------------
 async function showAlert(title, message) {
     var al = new Alert();
     al.title = title;
@@ -307,67 +342,53 @@ async function showAlert(title, message) {
     al.addCancelAction = "Cancel"
     al.present();
 }
+//---------------------------------------------
 
+
+//MAIN SETUP FUNCTION, OUTPUTS A VALUE THAT EITHER SHOWS SUCCESS (1), OR CANCELATION (-1)
+//---------------------------------------------
 async function setup() {
-    /*var a = new Alert();
+    var a = new Alert();
     a.title = "You will now be guided through set-up, starting with contact selection. Tap next."
     a.addAction("Next");
-    await a.present();*/
+    await a.present();
     var contact = await selectContact();
     if (contact[0] == null) {
         return -1
     } else {
-        console.log("contact" + contact);
         var cii = await addCheckInItems();
         if (cii == "") {
             return -1;
         } else {
-            await updateSettings(cii,contact,settings.checkInSheetPath,settings.bmAdded);
+            await updateSettings(cii, contact, settings.checkInSheetPath, settings.bmAdded);
             await changeNotificationTime();
             return 1
         }
     }
 }
+//---------------------------------------------
 
 //MAIN
+//Initialize variables
 var params = args.queryParameters
 var action = null
 if (params) {
     action = params.action;
-    console.log(action);
 }
-
 let settings = getSettings();
-console.log(settings);
+log("Settings: " + JSON.stringify(settings))
 const bookmarkName = "Checkin"
 if (settings.checkInItems == "") {
     action = "setup";
 }
-action = "setup";
-//createSettingsDefault()
 const PARTNER = Array.from(settings.accountabilityContact)[0];
-console.log(PARTNER)
-
-var fm = FileManager.local();
-var dir = fm.libraryDirectory();
-//console.log(fm.listContents(dir));
-var path = fm.joinPath(dir, "check_in_tracker_settings.txt");
-//fm.remove(path);
-
-//input the phone numbers of the people to whome you want your check-ins sent
-// repeat the below rows with anything that you want on your check-in
+log("PARTNER: " + PARTNER);
 const CHECK_IN_AREAS = settings.checkInItems;
-//createSettingsDefault();
-//let fm = await FileManager.iCloud(); 
-// build file manager
-//let path = await fm.bookmarkedPath("Check-in")
-//updateSettings(settings.checkInItems,settings.accountabilityContact,path);
-//getSettings();
+log("CHECK_IN_AREAS: " + CHECK_IN_AREAS);
 
-// settings options are change accountsbility contaxt, change check-in items, find check in spreadsheet, send check in soreadsheet, check in
+//  switch function to determin, from passed parameters which function to run
 switch (action) {
     case undefined:
-        console.log("Made it to run menu")
         await runMenu(settings);
         break;
     case "checkIn":
